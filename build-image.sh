edcb34224725f7a19e599cfc3076f55260db9460
Duc Tran <ducth@doitsu.tech>
2021-11-11 21:23:15 +0700

    latest

---
+++ build-image.sh
@@ -0,0 +1,6 @@
+Version=$(date '+%Y%m%d%H%M%S')
+Repository="my-posts-application"
+
+docker build . -t $Repository
+docker image tag $Repository "docker-hub.doitsu.tech/$Repository:latest"
+docker image tag $Repository "docker-hub.doitsu.tech/$Repository:$Tag"
