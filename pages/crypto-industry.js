68516c9e3ecfefdc94ca1e26b68ffcf0ceacdb3d
Duc Tran <ducth@doitsu.tech>
2021-11-13 16:06:56 +0700

    latest

--- pages/crypto-industry.js
+++ pages/crypto-industry.js
@@ -1,50 +1,38 @@
-import Link from "next/link";
 import Script from 'next/script'
-
+import { useState } from "react";
 
 export default function CryptoIndustry() {
+	const [state, setstate] = useState({
+		dotEcosystem: [
+			'polkadot',
+			'kusama',
+			'moonriver',
+			'karura',
+			'pha',
+			'rmrk',
+		]
+	})
+
 	return (
 		<>
-			<Script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js" strategy="beforeInteractive" />
-			<section className="container w-auto mx-auto flex flex-col items-center justify-center rounded-xl bg-gradient-to-b from-green-400 to-green-700 p-4">
-				<h1 className="sm:text-lg md:text-2xl font-bold py-4">King and Queen</h1>
-				<div className="flex flex-row justify-around p-4 w-full">
-					<div className="coinmarketcap-currency-widget"
-						data-currencyid="1"
-						data-base="USD"
-						data-secondary="BTC"
-						data-ticker="true"
-						data-rank="true"
-						data-marketcap="true"
-						data-volume="true"
-						data-statsticker="true"
-						data-stats="USD">
-					</div>
-					<div className="coinmarketcap-currency-widget"
-						data-currencyid="1027"
-						data-base="USD"
-						data-secondary="BTC"
-						data-ticker="true"
-						data-rank="true"
-						data-marketcap="true"
-						data-volume="true"
-						data-statsticker="true"
-						data-stats="USD">
-					</div>
-				</div>
+			<Script src="https://widgets.coingecko.com/coingecko-coin-heatmap-widget.js" strategy="beforeInteractive" />
+			<Script src="https://widgets.coingecko.com/coingecko-coin-price-chart-widget.js" strategy="beforeInteractive" />
+
+			<section className="mt-8 mx-auto sm:w-11/12 md:w-4/5 flex flex-col bg-none p-4">
+				<h1 className="sm:text-lg md:text-2xl font-bold py-4 text-center">Heat Map</h1>
+				<coingecko-coin-heatmap-widget className="w-full bg-none" height="1080" locale="en"></coingecko-coin-heatmap-widget>
 			</section>
-			<Script src="https://widgets.coingecko.com/coingecko-coin-compare-chart-widget.js" strategy="beforeInteractive" />
-			<section className="mt-8 mx-auto sm:w-11/12 md:w-4/5 flex flex-col rounded-xl bg-none p-4">
-				<div></div>
+
+			<section className="mt-8 mx-auto sm:w-11/12 md:w-4/5 bg-none p-4">
 				<h1 className="sm:text-lg md:text-2xl font-bold py-4 text-center">Dot and Kusama ecosystem charts</h1>
-				<br />
-				<coingecko-coin-compare-chart-widget coin-ids="polkadot" currency="usd" locale="en"></coingecko-coin-compare-chart-widget>
-				<br />
-				<coingecko-coin-compare-chart-widget coin-ids="kusama" currency="usd" locale="en"></coingecko-coin-compare-chart-widget>
-				<br />
-				<coingecko-coin-compare-chart-widget coin-ids="moonriver" currency="usd" locale="en"></coingecko-coin-compare-chart-widget>
-				<br />
-				<coingecko-coin-compare-chart-widget coin-ids="karura,pha" currency="usd" locale="en"></coingecko-coin-compare-chart-widget>
+				<div className="grid grid-cols-2">
+					{state.dotEcosystem.map(e => (
+						<div className="p-4">
+							<br />
+							<coingecko-coin-price-chart-widget coin-id={e} currency="usd" height="300" locale="en"></coingecko-coin-price-chart-widget>
+						</div>
+					))}
+				</div>
 			</section>
 		</>
 	);
