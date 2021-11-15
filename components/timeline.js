e1d0310a19e26013966bc5f18ee8d73850c6b8c8
Duc Tran <ducth@doitsu.tech>
2021-11-15 10:45:54 +0700

    latest

--- components/timeline.js
+++ components/timeline.js
@@ -53,48 +53,31 @@ class Timeline extends Component {
 					{this.state.lines.map((l, k) => {
 						const kPlusOne = k + 1;
 						const isFirst = kPlusOne === 1;
+						const isLeft = kPlusOne % 2 === 0;
 						return (
-							(k + 1) % 2 === 0
-								? <div key={k} className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-									<div className="order-1 w-5/12"></div>
-									<div className="z-20 flex items-center order-1 bg-gray-400 shadow-xl w-12 h-12 rounded-full">
-										{
-											<svg xmlns="http://www.w2.org/2000/svg" className="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16l-4-4m0 0l4-4m-4 4h18" />
-											</svg>
-										}
-
-									</div>
-									<div className="order-1 bg-gradient-to-tl from-green-100 to-green-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-										<h2 className="mb-1 font-bold text-black text-xl">{l.title}</h2>
-										<h3 className="mb-2 italic text-md">[{l.fromDate.format('YYYY-MM')} - {l.toDate.format('YYYY-MM')}]</h3>
-										<h3 className="mb-1 text-md">Work as a: </h3>
-										<ul className="mb-2 list-disc list-inside text-sm">
-											{l.roles.map((e, k) => (
-												<li key={k}>{e}</li>
-											))}
-										</ul>
-
-										<p className="text-sm font-medium leading-snug tracking-wide text-black text-opacity-100 text-justify">{l.content}</p>
-									</div>
-								</div>
-								: <div key={k}>
+							!isLeft
+								? <div key={k}>
 									<div className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border left-1/2"></div>
-									<div className="mb-8 flex justify-between items-center w-full right-timeline">
+									<div className="mb-8 md:flex md:justify-between md:items-center w-full sm:grid sm:grid-cols-1 sm:justify-items-center">
 										<div className="order-1 w-5/12"></div>
-										<div className={`z-20 flex items-center order-1 bg-gray-400 shadow-xl w-12 h-12 rounded-full ${isFirst && 'border-4 border-green-400 animate-bounce'}`}>
+										<div className={`z-20 flex items-center order-1 bg-gray-400 shadow-xl w-12 h-12 rounded-full sm:col-auto ${isFirst ? 'sm:mb-4' : 'sm:mb-8'} ${isFirst && 'border-4 border-green-400 animate-bounce'}`}>
 											{
 												isFirst ?
 													<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 														<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
 													</svg>
 													:
-													<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-														<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-													</svg>
+													<>
+														<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto hidden md:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+															<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+														</svg>
+														<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto hidden sm:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+															<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7l4-4m0 0l4 4m-4-4v18" />
+														</svg>
+													</>
 											}
 										</div>
-										<div className="order-1 bg-gradient-to-tr from-gray-100 to-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
+										<div className="order-1 bg-gradient-to-tr from-gray-100 to-gray-400 rounded-lg shadow-xl px-6 py-4 md:w-5/12 sm:w-full sm:z-20 sm:col-auto">
 											<h2 className="mb-1 font-bold text-gray-800 text-xl text-justify">{l.title}</h2>
 											<h3 className="mb-2 italic text-md">[{l.fromDate.format('YYYY-MM')} - {l.isWorking ? 'now' : l.toDate.format('YYYY-MM')}]</h3>
 											<h3 className="mb-1 text-md">Work as a: </h3>
@@ -105,6 +88,32 @@ class Timeline extends Component {
 											</ul>
 											<p className="mb-2 text-sm leading-snug tracking-wide text-gray-900 text-opacity-100 text-justify">{l.content}</p>
 										</div>
+									</div>
+								</div>
+								: <div key={k} className="mb-8 md:flex md:justify-between md:flex-row-reverse md:items-center w-full sm:grid sm:grid-cols-1 sm:justify-items-center">
+									<div className="order-1 w-5/12"></div>
+									<div className="z-20 flex items-center order-1 bg-gray-400 shadow-xl w-12 h-12 rounded-full sm:col-auto sm:mb-8">
+										{
+											<>
+												<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto hidden md:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+													<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+												</svg>
+												<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-auto hidden sm:block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+													<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7l4-4m0 0l4 4m-4-4v18" />
+												</svg>
+											</>
+										}
+									</div>
+									<div className="order-1 bg-gradient-to-tl from-green-100 to-green-400 rounded-lg shadow-xl px-6 py-4 md:w-5/12 sm:w-full sm:z-20 sm:col-auto">
+										<h2 className="mb-1 font-bold text-black text-xl">{l.title}</h2>
+										<h3 className="mb-2 italic text-md">[{l.fromDate.format('YYYY-MM')} - {l.toDate.format('YYYY-MM')}]</h3>
+										<h3 className="mb-1 text-md">Work as a: </h3>
+										<ul className="mb-2 list-disc list-inside text-sm">
+											{l.roles.map((e, k) => (
+												<li key={k}>{e}</li>
+											))}
+										</ul>
+										<p className="text-sm font-medium leading-snug tracking-wide text-black text-opacity-100 text-justify">{l.content}</p>
 									</div>
 								</div>
 						);
